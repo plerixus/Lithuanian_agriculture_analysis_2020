3fad93a6dde696a858f38acddd237c18dca0c693
plerixus <elijaszaremba@gmail.com>
2024-12-25 18:44:13 +0200

    limited analysis to smaller farms

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,6 +1,3 @@
-def hello():
-    return "Hello"
-
 
 def remove_exceptions_from_df(df):
     """
@@ -23,3 +20,33 @@ def remove_exceptions_from_df(df):
     )
     
     return df[~conditions]
+
+def prep_df_for_isolated_analysis(df, column_name):
+    """
+    Filters out rows where the values for specific columns are not 0, excluding the given column.
+    Removes columns that are not selected
+    Filters values where specific column is not 0
+
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
+        column_name (str): The column to exclude from the zero-check.
+
+    Returns:
+        pd.DataFrame: A filtered DataFrame.
+    """
+    columns_to_check = [
+        'f_2000_k',
+        'f_2008_k',
+        'f_2012_k',
+        'f_2015_k',
+        'f_2019_k',
+        'f_2029_k',
+        'f_2018_k',
+        'f_2031_k',
+        ]
+    columns_to_check = [col for col in columns_to_check if col != column_name]
+    filtered_df = df[(df[columns_to_check] == 0).all(axis=1)]
+    filtered_df = filtered_df.drop(columns=columns_to_check)
+    filtered_df = filtered_df[filtered_df[column_name] != 0]
+
+    return filtered_df
